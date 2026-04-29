1bfc81f3d45a44a006ac0aac7b69963795215e84
szp2005 <147570523+szp2005@users.noreply.github.com>
2026-04-29 09:49:30 -0700

    fix: remove sitemap integration from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://home-office-gear.pages.dev',
   output: 'static',
-  integrations: [sitemap()],
 });
